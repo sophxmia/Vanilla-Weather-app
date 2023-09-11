8a706b358bc52db6a8cec8f3ba8f348aef07d149
sophxmia <maliarenko.sofiia@gmail.com>
2023-09-11 23:51:09 +0300

    Improved visual style
    I added some CSS to my project in order to make it look pretty

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ function formatTime(timestamp) {
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   return days[day];
 }
 
